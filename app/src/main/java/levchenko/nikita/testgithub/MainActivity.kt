37c8c6f847351cbc01ab00187be98128264117f1
nikitalevchenko <levko07324@gmail.com>
2019-10-14 15:19:12 +0300

    Initial commit

---
+++ app/src/main/java/levchenko/nikita/testgithub/MainActivity.kt
@@ -0,0 +1,12 @@
+package levchenko.nikita.testgithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
